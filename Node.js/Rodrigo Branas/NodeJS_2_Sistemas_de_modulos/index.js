080c6034a693e8c98c99c5fbfd4d386f6ea9786f
Cristiane Ferreira <cristianedeferreira@gmail.com>
2017-09-16 12:28:40 -0300

    modules

--- Node.js/Rodrigo Branas/NodeJS_2_Sistemas_de_modulos/index.js
+++ Node.js/Rodrigo Branas/NodeJS_2_Sistemas_de_modulos/index.js
@@ -1,2 +1,4 @@
-var serialGenerator = require('./serialGenerator.js')
+var CreateSerialGenerator = require('./serialGenerator.js')
+var serialGenerator = CreateSerialGenerator()
+// var serialGenerator = require('./serialGenerator.js')
 console.log(serialGenerator.generate())
--- Node.js/Rodrigo Branas/NodeJS_2_Sistemas_de_modulos/serialGenerator.js
+++ Node.js/Rodrigo Branas/NodeJS_2_Sistemas_de_modulos/serialGenerator.js
@@ -1,6 +1,27 @@
-var max = 10000;
+var createSerialGenerator = function () {
 
-exports.generate = function (){
-    return Math.floor(Math.random() * max)
+    var max = 10000;
+
+    var generate = function () {
+        return Math.floor(Math.random() * max)
+    }
+    return {
+        generate: generate
+    }
 }
 
+module.exports = createSerialGenerator
+
+// module.exports = createSerialGenerator();
+
+
+// var SerialGenerator = function(){
+
+//     var max = 10000;
+
+//     this.generate = function(){
+//         return Math.floor(Math.random() * max)
+//     }
+// }
+
+// module.exports = new SerialGenerator()
